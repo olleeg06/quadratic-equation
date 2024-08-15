9404514a7179fa81c6dc2690d4a793585a7797ab
olleeg06 <dontsov12345678908@mail.ru>
2024-08-15 18:12:21 +0300

    Add files via upload

--- квадратка.c
+++ квадратка.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <math.h>
 
-int solve (double coefficient[3]);
+int solve (double coefficient[3], double answer[6], double discriminant);
 int input (double coefficient [3]);
 int formula (double * x_1, double * x_2, double coefficient[3], double discriminant);
 int formula_complex (double * x_real, double * x_imaginary, double coefficient[3], double discriminant);
@@ -9,54 +9,54 @@ int formula_small (double * x, double coefficient[3]);
 int formula_linear (double *x, double coefficient[3]);
 int discriminant_epsilon_plus (double discriminant, double epsilon);
 int discriminant_epsilon_minus (double discriminant, double epsilon);
+int show_answer(double coefficient[3], double answer[6], double discriminant);
+double formula_discriminant (double coefficient[3]);
 
 double const epsilon = 1e-11;
 
 int main(void)
 {   
-    printf("Enter the coefficients of the quadratic equation (or 'q' to completion):\n");
     double coefficient [3];
+    double answer [6];
+    double discriminant = 0;
+    
+    printf("Enter the coefficients of the quadratic equation (or 'q' to completion):\n");
 
     while (input(coefficient) == 3)
     {
+        discriminant = formula_discriminant(coefficient);
         printf("%lg * x^2 + %lg * x + %lg = 0 \n",  coefficient[0], coefficient[1] ,coefficient[2]);
-        solve(coefficient);
+        solve (coefficient, answer, discriminant);
+        show_answer (coefficient, answer, discriminant);
         printf("\nEnter the coefficients of the quadratic equation (or 'q' to completion):\n");
     }
     printf("\nHave a good day!");
  return 0;
 }
 
-int solve(double coefficient[3])
+double formula_discriminant(double coefficient[3])
 {
-        double discriminant = 0;
-        discriminant = coefficient[1] * coefficient[1] - 4 * coefficient[0] * coefficient[2];
+    return coefficient[1] * coefficient[1] - 4 * coefficient[0] * coefficient[2];
+}
+
+int solve(double coefficient[3], double answer[6], double discriminant)
+{
 
       if (coefficient[0] != 0 && discriminant_epsilon_plus (discriminant, epsilon))
     {
-        double solution_1, solution_2;
-        formula (&solution_1, &solution_2, coefficient, discriminant);
-        printf ("solutions to the equation: solution_1 = %lg, solution_2 = %lg", solution_1, solution_2);
+        formula (&answer[0], &answer[1], coefficient, discriminant);
 
     } else if (coefficient[0] != 0 && discriminant_epsilon_minus (discriminant, epsilon))
     {
-        double solution_real, solution_imaginary;
-        formula_complex (&solution_real, &solution_imaginary, coefficient, discriminant);
-        printf ("solutions to the equation: solution_1 = %lg + %lg * i, solution_2 = %lg - %lg * i",
-        solution_real, solution_imaginary,solution_real, solution_imaginary);
+        formula_complex (&answer[2], &answer[3], coefficient, discriminant);
 
     } else if (coefficient[0] != 0 && !(discriminant_epsilon_plus (discriminant, epsilon)) && !(discriminant_epsilon_minus (discriminant, epsilon))) 
     {
-        double solution_small;
-        formula_small (&solution_small, coefficient);
-        printf ("solutions to the equation: solution_1 = solution_2 = %lg", solution_small);
+        formula_small (&answer[4], coefficient);
 
     } else 
     {
-        double solution;
-        formula_linear (&solution, coefficient);
-        printf ("solution to the equation: solution = %lg", solution);
-
+        formula_linear (&answer[5], coefficient);
     }
     }
 
@@ -101,4 +101,26 @@ int discriminant_epsilon_minus (double discriminant, double epsilon)
     return 1;
     else 
     return 0;
+}
+
+int show_answer(double coefficient[3], double answer[6], double discriminant)
+{
+    if (coefficient[0] != 0 && discriminant_epsilon_plus (discriminant, epsilon))
+    {
+        printf ("solutions to the equation: solution_1 = %lg, solution_2 = %lg", answer[0], answer[1]);
+
+    } else if (coefficient[0] != 0 && discriminant_epsilon_minus (discriminant, epsilon))
+    {
+        printf ("solutions to the equation: solution_1 = %lg + %lg * i, solution_2 = %lg - %lg * i",
+        answer[2], answer[3], answer[2], answer[3]);
+
+    } else if (coefficient[0] != 0 && !(discriminant_epsilon_plus (discriminant, epsilon)) && !(discriminant_epsilon_minus (discriminant, epsilon))) 
+    {
+        printf ("solutions to the equation: solution_1 = solution_2 = %lg", answer[4]);
+
+    } else 
+    {
+        printf ("solution to the equation: solution = %lg", answer[5]);
+
+    }
 }
